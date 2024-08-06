a3766538c57e10ced5662ea64608c5b2c20e122a
hariprem@agamworks.com <hariprem@agamworks.com>
2024-08-07 00:33:42 +0530

    upto event handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,108 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
+  selector: 'app-user',
+  template: `
+    Username: {{ username }}
+  `,
+  standalone: true,
+})
+export class UserComponent {
+  username = 'John Doe';
+}
+
+@Component({
+  selector: 'app-if',
+  template: `@if(correct){
+      <h3>If Statement</h3>
+      }
+      @else{
+      <h3>Else Statement</h3>
+      }`,
+  standalone: true
+})
+
+export class IfComponent {
+  correct = false;
+}
+
+@Component({
+  selector: 'app-for',
+  template: `
+    @for(item of sample; track item.name) {
+      <p>{{ item.name }}</p>
+    }
+  `,
+  standalone: true,
+})
+export class ForComponent {
+  sample = [{name: 'John'}, {name: 'Doe'}, {name: 'Jane'}, {name: 'Fly'}];
+}
+
+@Component({
+  selector: 'app-event',
+  template: `
+    <section>
+      <button (click)="onClick()">Click Me</button>
+      <p>{{ click }}</p>
+    </section>
+
+    <section (mouseover)="onMouseOver()">
+      <p>There's a secret message for you, hover to reveal:
+      {{ message }}</p>
+    </section>
+
+  `,
+  standalone: true
+})
+export class EventComponent{
+  click=''
+  message='';
+  onClick() {
+    this.click='Event Triggered! 🏄';
+  }
+
+  onMouseOver() {
+    this.message='🎩 👑';
+  }
+}
+
+
+@Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [
+    RouterOutlet,
+    UserComponent,
+    IfComponent,
+    ForComponent,
+    EventComponent
+  ],
+  template: `
+  Hello Universe! A ping from{{ city }} {{1+1}}!
+  <section>
+    <app-user></app-user>
+  </section>
+  <section>
+    <app-if></app-if>
+  </section>
+    <section>
+    <app-for></app-for>
+  </section>
+  <div [contentEditable]="canEdit">
+    <p>Hello Edit!</p>
+  </div>
+  <div>
+    <app-event></app-event>
+  <div></div>
+  `,
+  styles: `
+  :host {
+    color: var(--gray-700);
+  }`,
 })
 export class AppComponent {
   title = 'demo-ang';
+  city = 'New York';
+  canEdit = true;
 }
